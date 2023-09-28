8e1348ebf0503211620fcda6f90adb8ee4fce625
eric <akaerickoh@gmail.com>
2023-09-28 21:42:45 +0800

    fixed bg animation

--- components/HelloWorld/index.tsx
+++ components/HelloWorld/index.tsx
@@ -6,7 +6,6 @@ export const HelloWorld = () => {
   return (
     <TypeAnimation
       cursor={false}
-      className="hello"
       sequence={[
         `I am a highly driven and passionate web developer with over 10 years of successful development of interactive web and mobile applications.\n
 Proven record of escalating customer satisfaction and increasing user interaction through timely implementation of cutting-edge designs, cutting development time, and integration of third-party services.`,
