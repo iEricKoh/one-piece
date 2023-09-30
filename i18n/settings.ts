65c32c68709f57b409da815c6a7cc7bfb87c5d95
eric <akaerickoh@gmail.com>
2023-09-30 17:45:50 +0800

    fixed loading

--- i18n/settings.ts
+++ i18n/settings.ts
@@ -6,18 +6,16 @@ export type LocaleTypes = (typeof locales)[number]
 export const defaultNS = "common"
 export const cookieName = "i18next"
 export const headerName = "x-next-locale"
-import resourcesToBackend from "i18next-resources-to-backend"
 
 export function getOptions(lang = fallbackLng, ns = defaultNS): InitOptions {
   return {
-    // debug: true, // Set to true to see console logs
     supportedLngs: locales,
     fallbackLng,
     lng: lang,
     fallbackNS: defaultNS,
     defaultNS,
     ns,
-    // debug: true,
+    debug: true,
     load: "currentOnly",
   }
 }
